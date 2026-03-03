e2f06ad14bd994b13c104d1b0d664dbd245504f4
Mateus <mateus.rosengartenn@gmail.com>
2026-03-03 15:44:44 -0300

    feat:add underline on navlinks hover

--- src/components/navLink.jsx
+++ src/components/navLink.jsx
@@ -9,7 +9,7 @@ function NavLinkItem({ to, children }) {
   return (
     <span
       onClick={handleScroll}
-      className="fs-custom me-2 font-primary text-decoration-none text-brand cursor-pointer"
+      className="navlink-item fs-custom me-2 font-primary text-decoration-none text-brand cursor-pointer"
     >
       {children}
     </span>
